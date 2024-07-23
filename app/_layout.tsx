bf5dc33be4d0313cc6273a8b283d4e44f23b47c9
Jazzik <127020898+Jazzik@users.noreply.github.com>
2024-07-23 18:25:20 +0500

    minor changes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,27 +6,28 @@ console.log('_layout.tsx');
 
 export default function Layout() {
   return (
-    <Stack screenOptions={{
+    <Stack screenOptions={{ 
       headerTitle: () => (
-        <View style={{ flexDirection: 'row', alignItems: 'center', flex: 1, marginRight: 30 }}> 
+        <View style={{ flexDirection: 'row', alignItems: 'center', flex: 1, marginRight: 40 }}> 
           <View style={{ flex: 1 }} /> 
           <View style={{
             width: 40, // Circle size
             height: 40, // Circle size
-            borderRadius: 25, // Half of the width/height to make it a circle
+            borderRadius: 20, // Half of the width/height to make it a circle
             backgroundColor: 'red', // Circle color
             justifyContent: 'center', // Center the image vertically
             alignItems: 'center', // Center the image horizontally
           }}>
             <Image
-              source={require('../assets/images/favicon.png')}
-              style={{ width: 30, height: 30 }}
+              source={require('@/assets/images/user/default-photo.png')}
+              style={{ width: 40, height: 40,  borderRadius: 20}}
             />
           </View>
         </View>
-      )
+      ), 
+      
     }}>
-      <Stack.Screen name="(tabs)"  options={{ headerShown: true, headerStyle: { backgroundColor: Colors.dark.upper_background } }} />
+      <Stack.Screen name="(tabs)"  options={{ headerShown: true, headerStyle: { backgroundColor: Colors.dark.upper_background, } }} />
     </Stack>
   );
 }
