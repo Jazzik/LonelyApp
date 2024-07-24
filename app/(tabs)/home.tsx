13d50e1da8b30ac81d308cc4b4b7278929927025
Jazzik <127020898+Jazzik@users.noreply.github.com>
2024-07-24 21:28:59 +0500

    loading anim

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,22 +1,29 @@
-import Animated, {FadeIn, FadeOut, useAnimatedStyle,useAnimatedScrollHandler, useSharedValue ,withSpring} from 'react-native-reanimated';
-import CircleButton from '../../components/CircleButton';
-import { StatusBar, StatusBarStyle } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, View, Dimensions, Button, Text } from 'react-native';
-import { ChallengeBar } from '@/components/ChallengeBar';
-import axios from 'axios';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useEffect, useState } from 'react';
-import { Colors } from '@/constants/Colors';
+import Animated, {
+  FadeIn,
+  FadeOut,
+  useAnimatedStyle,
+  useAnimatedScrollHandler,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated";
+import CircleButton from "../../components/CircleButton";
+import { StatusBar, StatusBarStyle } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, View, Dimensions, Button, Text } from "react-native";
+import { ChallengeBar } from "@/components/ChallengeBar";
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useEffect, useState } from "react";
+import { Colors } from "@/constants/Colors";
 // import { StackNavigationProp } from '@react-navigation/stack';
-import { useNavigation } from '@react-navigation/native'; // Import useNavigation
-import CheckInternetConnection from '@/components/CheckInternetConnection';
-import {ip} from '@/ip.json'
-import { Punkboy1 } from '@/components/characters/punkboy/punkboy';
-import { ActivityIndicator } from 'react-native';
+import { useNavigation } from "@react-navigation/native"; // Import useNavigation
+import CheckInternetConnection from "@/components/CheckInternetConnection";
+import { ip } from "@/ip.json";
+import { Punkboy1 } from "@/components/characters/punkboy/punkboy";
+import { ActivityIndicator } from "react-native";
 export default function Tab() {
-  const nav = useNavigation()
-  const { width, height } = Dimensions.get('window');
+  const nav = useNavigation();
+  const { width, height } = Dimensions.get("window");
   const translationY = useSharedValue(0);
   const scrollY = useSharedValue(0);
   const [data, setData] = useState([]);
@@ -25,109 +32,133 @@ export default function Tab() {
 
   const scrollHandler = useAnimatedScrollHandler((event) => {
     scrollY.value = event.contentOffset.y;
-
   });
   useEffect(() => {
-    fetchData();},[]);
+    fetchData();
+  }, []);
 
-    const fetchData = async () => {
-        const token = await AsyncStorage.getItem("userToken")
-        console.log("tut")
-        // const token = await AsyncStorage.removeItem("userToken")
-        setIsInternetError(false); // Reset internet error state
-        await axios.get(`http://${ip}:8080/tasks/groups/en-en`,
-          {"headers":{"Authorization":"Bearer "+ token}}).then((response) => {
-            const result = response.data;
-            console.log(result, "result");
-            setData(result);
-          }).catch((error) => {
-
-            setIsInternetError(true);
-            //error code 
-            console.log("error");
-            // console.log(JSON.stringify(error));  // for full error data
-            // console.error(error.data);
-          }).finally(() => {
-            setLoading(false);
-          });
-        
-      }
+  const fetchData = async () => {
+    const token = await AsyncStorage.getItem("userToken");
+    // const token = await AsyncStorage.removeItem("userToken")
+    setLoading(true);
+    setIsInternetError(false); // Reset internet error state
+    console.log("sending request");
+    await axios
+      .get(`http://${ip}:8080/tasks/groups/en-en`, {
+        headers: { Authorization: "Bearer " + token },
+      })
+      .then((response) => {
+        const result = response.data;
+        console.log(result, "result");
+        setData(result);
+      })
+      .catch((error) => {
+        setIsInternetError(true);
+        console.log("error");
+        // console.log(JSON.stringify(error));  // for full error data
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
 
   const animatedStyle = useAnimatedStyle(() => {
-  return {
-    transform: [{ translateY: -scrollY.value }],
-  };
+    return {
+      transform: [{ translateY: -scrollY.value }],
+    };
   });
 
   const retryConnection = () => {
     fetchData();
   };
   return (
-
     <View style={styles.container}>
-    
-      <Animated.ScrollView
-
-        onScroll={scrollHandler}
-        scrollEventThrottle={16}
-        style={styles.scrollView}
-      >
-        {isInternetError ? (
-            <View style= {{flex:1, alignItems: 'center', justifyContent: 'center', height: 600}}>
+      {loading ? (
+        <Animated.ScrollView
+          onScroll={scrollHandler}
+          scrollEventThrottle={16}
+          style={styles.loadingContainer}
+          contentContainerStyle={{ flexGrow: 1 }}
+        >
+          <View
+            style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+          >
+            <ActivityIndicator size="large" color={Colors.dark.text} />
+          </View>
+        </Animated.ScrollView>
+      ) : (
+        <Animated.ScrollView
+          onScroll={scrollHandler}
+          scrollEventThrottle={16}
+          style={styles.scrollView}
+        >
+          {isInternetError ? (
+            <View
+              style={{
+                flex: 1,
+                alignItems: "center",
+                justifyContent: "center",
+                height: 600,
+              }}
+            >
               <Punkboy1 />
               <CheckInternetConnection />
               <Button title="Retry" onPress={retryConnection} />
             </View>
           ) : (
-        <Animated.View style={[styles.content, animatedStyle]}>
-            
-                  <Animated.View 
-                    entering={FadeIn.duration(100)} 
-                    // exiting={FadeOut.duration(1000)} 
-                    style={styles.container}>
-                      {data.map((item, index) => (
-                        <ChallengeBar key={index} title={item} progress={0}></ChallengeBar>
-                      ))}
-                  </Animated.View>
-        
-          
-          
-
-         </Animated.View>
-       )}
+            <Animated.View style={[styles.content, animatedStyle]}>
+              <Animated.View
+                entering={FadeIn.duration(100)}
+                // exiting={FadeOut.duration(1000)}
+                style={styles.container}
+              >
+                {data.map((item, index) => (
+                  <ChallengeBar
+                    key={index}
+                    title={item}
+                    progress={0}
+                  ></ChallengeBar>
+                ))}
+              </Animated.View>
+            </Animated.View>
+          )}
         </Animated.ScrollView>
-      
-      <StatusBar style='light'/>
-  </View>
+      )}
+      <StatusBar style="light" />
+    </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: Colors.dark.background,
     flex: 1,
-    position: 'relative', // Ensure the container is relative
+    position: "relative", // Ensure the container is relative
   },
   buttonContainer: {
-    position: 'absolute',
+    position: "absolute",
+  },
+  loadingContainer: {
+    flex: 1,
+    // backgroundColor: 'red',
   },
   scrollView: {
     flex: 1,
   },
   content: {
-    flex:1,
+    flex: 1,
     flexGrow: 1,
     padding: 20,
   },
   item: {
     height: 100,
     marginBottom: 20,
-    backgroundColor: '#f9c2ff',
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: "#f9c2ff",
+    justifyContent: "center",
+    alignItems: "center",
   },
   itemText: {
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
 });
