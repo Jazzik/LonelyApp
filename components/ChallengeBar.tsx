ad3599523b607849b230f52e36d6a872a52dc403
Jazzik <127020898+Jazzik@users.noreply.github.com>
2024-07-24 21:37:16 +0500

    fix

--- components/ChallengeBar.tsx
+++ components/ChallengeBar.tsx
@@ -1,11 +1,27 @@
 import { Colors } from "@/constants/Colors";
 import { useState } from "react";
-import { View, Text, StyleSheet, TouchableWithoutFeedback, Button, Pressable } from "react-native";
-import Animated, { useSharedValue, useAnimatedStyle } from "react-native-reanimated";
-import * as Haptics from 'expo-haptics';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  Button,
+  Pressable,
+} from "react-native";
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+} from "react-native-reanimated";
+import * as Haptics from "expo-haptics";
 import { useNavigation } from "expo-router";
 
-export function ChallengeBar({ progress, title }: { progress: number; title: string }) {
+export function ChallengeBar({
+  progress,
+  title,
+}: {
+  progress: number;
+  title: string;
+}) {
   const [isEnabled, setIsEnabled] = useState(false);
   const scale = useSharedValue(1);
   const navigation = useNavigation();
@@ -16,47 +32,42 @@ export function ChallengeBar({ progress, title }: { progress: number; title: str
     };
   });
   return (
-
-    <Pressable 
-    onPressIn={() => {
-      navigation.navigate("tasks")
-      scale.value = 0.95;
-    }}
-    onPressOut={() => {
- 
-      scale.value = 1;
-     
-    }}
-    onLongPress={() => {
-
-      // Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
-      scale.value = 0.95;
-      setTimeout(() => {
-        scale.value = 0.93;
-      }, 100);
-      setTimeout(() => {
-        scale.value = 0.91;
-      }, 100);
-      setTimeout(() => {
-        scale.value = 0.89;
-      }, 100);
-      setTimeout(() => {
-        scale.value = 0.87;
-      }, 100);
-
-    }}
-    onPress={() => {
-  
-      Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
-      scale.value = 0.90;
-      
-      //wait 1 second
-      setTimeout(() => {
+    <Pressable
+      onPressIn={() => {
+        scale.value = 0.95;
+      }}
+      onPressOut={() => {
         scale.value = 1;
+      }}
+      onLongPress={() => {
+        // Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+        scale.value = 0.95;
+        setTimeout(() => {
+          scale.value = 0.93;
+        }, 100);
+        setTimeout(() => {
+          scale.value = 0.91;
+        }, 100);
+        setTimeout(() => {
+          scale.value = 0.89;
+        }, 100);
+        setTimeout(() => {
+          scale.value = 0.87;
+        }, 100);
+      }}
+      onPress={() => {
         Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
-      }, 30);
-      
-    }}>
+        scale.value = 0.9;
+
+        //wait 1 second
+        setTimeout(() => {
+          scale.value = 1;
+          Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+        }, 30);
+
+        navigation.navigate("tasks");
+      }}
+    >
       <Animated.View style={[styles.container, animatedStyle]}>
         <Text style={styles.label}>{title}</Text>
         <View style={styles.progressBarContainer}>
@@ -64,19 +75,18 @@ export function ChallengeBar({ progress, title }: { progress: number; title: str
             style={[
               styles.progressBar,
               { width: `${progress}%` },
-              { backgroundColor: isEnabled ? Colors.dark.text : Colors.dark.background },
+              {
+                backgroundColor: isEnabled
+                  ? Colors.dark.text
+                  : Colors.dark.background,
+              },
             ]}
           />
         </View>
-
-        
-        
-     
-    </Animated.View>
+      </Animated.View>
     </Pressable>
   );
-};
-
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -88,29 +98,28 @@ const styles = StyleSheet.create({
   },
   label: {
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 10,
     color: Colors.dark.text,
   },
   progressBarContainer: {
-    width: '100%',
+    width: "100%",
     height: 10,
     backgroundColor: Colors.dark.text,
     borderRadius: 5,
-    overflow: 'hidden',
+    overflow: "hidden",
     marginBottom: 20,
   },
   progressBar: {
-    height: '100%',
+    height: "100%",
     borderRadius: 5,
   },
   switchContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
   switchLabel: {
     fontSize: 16,
   },
-})
+});
