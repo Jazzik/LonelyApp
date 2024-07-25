050f240b7581b5c1102fdc9934c6a04eeb6a5d39
Aziz <your.email@example.com>
2024-07-25 19:04:22 +0200

    DAaaaaaa

--- api/apiv1.js
+++ api/apiv1.js
@@ -3,7 +3,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { ip } from "@/ip.json";
 import { isExpired } from "@/utils/token";
 export async function getGroups(lang){
-    refreshTokenIfExpired()
+    await refreshTokenIfExpired()
     const token = await AsyncStorage.getItem("accessToken");
     return await axios
       .get(`http://${ip}:8080/api/v1/tasks/groups/${lang}`, {
@@ -14,23 +14,28 @@ export async function getGroups(lang){
 export async function refreshTokenIfExpired(){
     const token = await AsyncStorage.getItem("accessToken");
     const refreshToken = await AsyncStorage.getItem("refreshToken");
-    console.log(isExpired(token))
     if(isExpired(token)){
+    console.log(refreshToken)
+
         const data = {
             accessToken: token,
             refreshToken: refreshToken 
           };
 
-        resp = await fetch(`http://${ip}:8080/api/v1/tokens/refresh`, {
+         resp = await fetch(`http://${ip}:8080/api/v1/tokens/refresh`, {
             method: 'POST', // Specify the request method
             headers: {
                 'Content-Type': 'application/json' // Set the content type to JSON
               },
             body: JSON.stringify(data) // Convert the data to a JSON string
             
-          }).then(data => {
-            console.log('Success:', data.accessToken)})
-        console.log(resp)
+          })
+          const responseData = await resp.json(); // Await the promise here
+          const newAccessToken = responseData.accessToken;
+          console.log(responseData.accessToken)
+          await AsyncStorage.setItem("accessToken", newAccessToken);
+
+
     }
 
 
