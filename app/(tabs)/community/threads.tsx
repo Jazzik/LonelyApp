7f34c72136ff53c6a611553f20d873415cbb5898
Aziz <your.email@example.com>
2024-08-23 15:17:30 +0200

    Added events

--- app/(tabs)/community/threads.tsx
+++ app/(tabs)/community/threads.tsx
@@ -2,24 +2,25 @@ import React, { useEffect, useState } from 'react';
 import { View, Text } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { GiftedChat } from 'react-native-gifted-chat';
-
+import { Dict } from 'i18n-js';
+import { eventEmitter } from '@/api/apiv1';
 export default function ThreadsScreen() {
-  const [messages, setMessages] = useState([]);
+  const [messages, setMessages] = useState<[]>();
 
 
   useEffect(() => {
     const fetchData = async () => {
+      eventEmitter.on("message", (message:String)=>console.log(message))
       const messagesString = await AsyncStorage.getItem('messages');
       const msgs = messagesString ? JSON.parse(messagesString) : [];
+
       setMessages(msgs.reverse()); // Update loading state after setting messages
     };
 
     fetchData();
-  }, [messages]);
-  if (messages.length <= 0) {
-    return <Text>Loading...</Text>; // Display a loading indicator while fetching data
-  }
-
+  }, []);
+  
+  if (messages!=undefined) {
   return (
     <View style={{ height: '100%' }}>
       <GiftedChat
@@ -35,4 +36,5 @@ export default function ThreadsScreen() {
       />
     </View>
   );
+}
 }
